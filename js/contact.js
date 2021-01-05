83b9686a9fa0b3a2f1e36cdeb944fc7527f31f06
Alexis <azen0x.alexis@gmail.com>
2021-01-05 22:44:50 +0100

    FormData

--- js/contact.js
+++ js/contact.js
@@ -1,7 +1,7 @@
 let form = document.querySelector("#contactForm");
 form.addEventListener("submit", function(e){
     e.preventDefault();
-    let data = new FormData(e.currentTarget);
+    let data = new FormData(e.target);
 
     console.log(data);
 
