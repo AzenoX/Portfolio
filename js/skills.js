ed8df8e50ce2047b6b2efaca95a46fa2e8def8a3
Alexis <azen0x.alexis@gmail.com>
2021-01-05 17:00:00 +0100

    hgjhfd

--- js/skills.js
+++ js/skills.js
@@ -15,10 +15,6 @@ fetch('json/skills.json')
     .then((response) => {
         skills_svg.forEach(function(ssvg){
             ssvg.addEventListener("click", function(){
-                let skills_explainations = document.querySelector('#skills--explainations');
-
-                let svg = document.querySelector('#skills--explainations__svg');
-                let svg_path = document.querySelector('#skills--explainations__svg path');
                 let title = document.querySelector('#skills--explainations__title');
                 let desc = document.querySelector('#skills--explainations__desc');
 
@@ -84,63 +80,6 @@ fetch('json/skills.json')
 
                     }
                 );
-
-
-                // tl.to(html, {morphSVG:"#css"}, "+=1")
-                //     .to(html, {morphSVG:"#js"}, "+=1")
-                //     .to(html, {morphSVG:"#jquery"}, "+=1")
-                //     .to(html, {morphSVG:"#php"}, "+=1")
-                //     .to(html, {morphSVG:"#sass"}, "+=1")
-                //     .to(html, {morphSVG:"#vue"}, "+=1")
-                //     .to(html, {morphSVG:html}, "+=1");
-
-
-
-
-                // let opacity = 1;
-                // let fadeOut = setInterval(function(){
-                //
-                //     skills_explainations.style.opacity = opacity + '';
-                //
-                //
-                //     //Fade out
-                //     opacity -= 0.05;
-                //     if(opacity < 0){
-                //         svg_path.setAttribute('d', target.path);
-                //         svg_path.setAttribute('fill', target.color);
-                //         title.innerText = target.title;
-                //
-                //         desc.innerText = '';
-                //         new Typed(desc, {
-                //             blink: true,
-                //             printErrors: false,
-                //             blinkClasses: ["blinker"],
-                //             blinkSpeed: 600
-                //         })
-                //             .type(target.description, {}, 10)
-                //             .run();
-                //
-                //
-                //         opacity = 0;
-                //         let fadeIn = setInterval(function(){
-                //
-                //             //Fade In
-                //             skills_explainations.style.opacity = Math.round(opacity) + '';
-                //
-                //
-                //             //Ptite animation translate au changement
-                //             svg.style.transform = 'translateX(' + ((1 - Math.round(opacity)) * 30) + '%)';
-                //             title.style.transform = 'translateX(-' + ((1 - Math.round(opacity)) * 30) + '%)';
-                //             desc.style.transform = 'translateY(-' + ((1 - Math.round(opacity)) * 30) + '%)';
-                //
-                //             opacity += 0.05;
-                //             if(Math.round(opacity) > 1){
-                //                 clearInterval(fadeIn);
-                //             }
-                //         },10);
-                //         clearInterval(fadeOut);
-                //     }
-                // },10);
 
             });
         });
