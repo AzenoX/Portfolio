0b1d32b4316f7b18d3aa1448a5d88190dca5ebff
Alexis <azen0x.alexis@gmail.com>
2021-01-06 09:39:34 +0100

    Changed to XMLHttpRequest

--- js/contact.js
+++ js/contact.js
@@ -7,9 +7,18 @@ form.addEventListener("submit", function(e){
     let message = document.querySelector("#message").value;
 
 
-    fetch("../php/sendMail.php",
-        {
-            body: `email=${email}&subject=${subject}&message=${message}`,
-            method: "post"
-        }).then((response) => console.log(response));
+    // fetch("../php/sendMail.php",
+    //     {
+    //         body: `email=${email}&subject=${subject}&message=${message}`,
+    //         method: "post"
+    //     }).then((response) => console.log(response));
+
+    let xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function(){
+        if(this.readyState === XMLHttpRequest.DONE && this.status === 200){
+            console.log('Sent!');
+        }
+    }
+    xhr.open("POST", "../php/sendMail.php");
+    xhr.send("email="+email+"&subject="+subject+"&message="+message);
 })
